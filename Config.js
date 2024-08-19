e8cc468d2b24e7670d14c4e5528bd63feb0c26d6
OLDTAJAY <tarajaebrown88@gmail.com>
2024-08-19 16:13:14 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,25 +1,97 @@
-//Bot by jermainetech//
+const { Sequelize } = require("sequelize");
+
+const fs = require("fs");
+
+require('dotenv').config();
 
 
-require("./all/module")
-global.owner = "18764793813" 
-global.namaCreator = "jermainetech"
-global.autoJoin = false 
-global.antilink = false 
-global.versisc = '6.𝟏' 
-global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
-global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
-global.sessionName = 'session'
-global.thumb = fs.readFileSync("./thumb.png") 
-global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "ROYAL-BOYKAH-263"
-global.author = "jermainetech" 
-global.jumlah = "5" 
-global.namabot = "ROYAL-BOYKAH-263" 
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update ${__filename}`))
-	delete require.cache[file]
-	require(file)
-})
+
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env', override: true });
+
+
+
+// Function to convert text to boolean
+
+function convertToBool(text, fault = 'true') {
+
+    return text === fault ? true : false;
+
+}
+
+
+
+// Function to convert string to boolean
+
+const toBool = (x) => (x && x.toLowerCase() === 'true') || false;
+
+global.apiUrl = 'https://api.maskser.me/'
+
+global.eypzApi = 'https://api.eypz.c0m.in/'
+
+
+
+// Define the Sequelize instance based on DATABASE_URL
+
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL;
+
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+
+// Export configuration variables
+
+module.exports = {
+
+  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
+
+  BRANCH: "main",
+
+  MODE: (process.env.MODE || 'private').toLowerCase(),
+
+  ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
+
+  LOG_MSG: toBool(process.env.LOG_MSG) || true,
+
+  READ_CMD: toBool(process.env.READ_CMD),
+
+  SESSION_ID: process.env.SESSION_ID || "izumi~pTKLfgmu",
+
+  MENU_URL: process.env.MENU_URL || "https://telegra.ph/file/06626ef7c51d90f3c19b8.jpg",
+
+  CAPTION: process.env.CAPTION || "♛ᴿᴼᵞᴬᴸ›™-263",
+
+  READ_MSG: toBool(process.env.READ_MSG),
+
+  OWNER_NAME: process.env.OWNER_NAME || "♛ᴿᴼᵞᴬᴸ›™-263",
+
+  BOT_NAME: process.env.BOT_NAME || "♛ᴿᴼᵞᴬᴸ›™-263",
+
+  SUDO: process.env.SUDO || null,
+
+  LANG: process.env.LANGUAGE === undefined ? 'EN' : process.env.LANGUAGE.toUpperCase(),
+
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || "♛ᴿᴼᵞᴬᴸ›™-263,❤️",
+
+  AUDIO_DATA: process.env.AUDIO_DATA || "ROYAL;♛ᴿᴼᵞᴬᴸ›™-263-ᴠ3;https://telegra.ph/file/06626ef7c51d90f3c19b8.jpg",
+
+  PROCESSNAME: process.env.PROCESSNAME || "♛ᴿᴼᵞᴬᴸ›™-263",
+
+  AUTHOR: process.env.AUTHOR || "♛ᴿᴼᵞᴬᴸ›™-263",
+
+  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
+
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+
+  HEROKU: toBool(process.env.HEROKU) || false,
+
+  TERMUX: toBool(process.env.TERMUX) || false,
+
+  DATABASE_URL: DATABASE_URL,
+
+  DATABASE:
+
+       DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
+
+  DEBUG: DEBUG
+
+};
